a33120450f5608d6091b8bf01e9da20e47905adc
Nuzulurrahmah <105371121+Nuzulurrahmah@users.noreply.github.com>
2022-09-10 17:05:18 +0700

    Tugas Nomor 4 Praktikum 2 selesai

---
+++ Praktikum 2/Prak2No4.java
@@ -0,0 +1,15 @@
+//Nama  : Nuzulurrahmah
+//NIM   : 2108107010012
+
+public class Prak2No4{
+public static void main(String[] args){
+
+    int i;
+    for (i=1;i<=20;i++){
+      if (i%2==0){
+      System.out.println(i);
+      }
+    }
+
+}
+}
