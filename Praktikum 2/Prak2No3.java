f9c28169218fa7cec4f991fac05521551ebcfcb6
Nuzulurrahmah <105371121+Nuzulurrahmah@users.noreply.github.com>
2022-09-10 17:01:25 +0700

    Tugas Nomor 3 Praktikum 2 selesai

---
+++ Praktikum 2/Prak2No3.java
@@ -0,0 +1,15 @@
+//Nama  : Nuzulurrahmah
+//NIM   : 2108107010012
+
+public class Prak2No3{
+            public static void main(String args[]) {
+
+            for (int i=0; i<5; i++)
+            {
+                for (int j=6; j>i+1; j--)
+                System.out.print('*');
+                System.out.println();
+            }
+
+        }
+        }
